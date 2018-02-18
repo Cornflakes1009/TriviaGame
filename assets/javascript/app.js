8b55cdd13aead7124b3d09a879f02ce689264cd9
Harold Davidson <harold.davidson89@gmail.com>
2018-02-17 18:29:17 -0600

    just need to do reset button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
 
     var timer = 15;
+    var startTimer;
     var correct = 0;
     var incorrect = 0;
+    var timedOut = 0;
+    var gifCounter = 0;
+    var questionCounter = 0;
+    var delay;
+
+    var gifArrCorrect = ["assets/images/happy-snape.gif", "assets/images/dumbledore.gif", "assets/images/rawr.gif", "assets/images/invisible.gif", "assets/images/boom.gif", "assets/images/flying.gif"];
+    var gifArrWrong = ["assets/images/voldemort.gif", "assets/images/explosion-hermione.gif", "assets/images/umbridge.gif", "assets/images/slappy-snape.gif", "assets/images/obviously.gif", "assets/images/werewolf.gif"];
 
     function Card(question, answer, choices) {
         this.question = question;
         this.answer = answer;
         this.choices = choices;
     }
-    var gifArrCorrect = ["assets/images/happy-snape.gif"];
-    var gifArrWrong = ["assets/images/voldemort.gif"];
 
     var questionOne = new Card("Who was Ginny Weasley's first boyfriend?", 'Michael Corner', ['Harry Potter', 'Dean Thomas', "Neville Longbottom", 'Michael Corner']);
 
@@ -25,15 +31,14 @@ $(document).ready(function () {
     var questionSix = new Card("What type of car is Mr Weasley's flying car?", 'Ford Anglia', ['Rolls Royce', 'Mini Cooper', 'Ford Anglia', 'Volkswagen Beetle']);
 
     var questions = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix];
-    var questionCounter = 0;
 
     function setQA() {
+        $('.answer-button').css("display", "inline-block");
         $('.question-text').html(questions[questionCounter].question);
         $('.answer-text1').html(questions[questionCounter].choices[0]);
         $('.answer-text2').html(questions[questionCounter].choices[1]);
         $('.answer-text3').html(questions[questionCounter].choices[2]);
         $('.answer-text4').html(questions[questionCounter].choices[3]);
-        ///// - content below from start button clicked
         $('.main-content').css("background-color", "rgb(26, 32, 40)");
         $('.start-button').css("display", "none");
         $('.game-section').css("display", "block");
@@ -41,58 +46,75 @@ $(document).ready(function () {
         $('.right-or-wrong').html("");
         startTimer = setInterval(function () {
             if (timer === 0) {
-
+                gifCounter++;
+                questionCounter++;
+                timedOut++;
                 stopTimer();
                 $('.gif-area').css("display", "inline-block");
-                $('.gif-area').attr("src", gifArrWrong[0]);
-                //timer++; // added extra 1 because timer was going down to 14 seconds
+                $('.answer-button').css("display", "none");
+                $('.gif-area').attr("src", gifArrWrong[gifCounter]);
                 restartQA();
                 console.log('break');
-
-
             }
             timer--; $('#time-counter').html(timer);
         }, 1000);
     }
     function restartQA() {
-        $('.right-or-wrong').html("Sorry. You're out of time. The answer was " + questions[0].answer);
+        $('.right-or-wrong').html("Sorry. You're out of time. The answer was " + questions[questionCounter - 1].answer + ".");
         setTimeout(function () { setQA(); }, 5000);
     }
-    var startTimer;
     // stops timer, sets time back to 30, and updates html
     function stopTimer() {
+        timer = 15;
         clearInterval(startTimer);
-        timer = 16;
         $('#time-counter').html(timer);
     }
-
     $('.start-button').on('click', function () {
         setQA();
         // remove first index of array after each question
     }) // end of start button being clicked
 
-
     $('.answer-button').on('click', function () {
         var buttonClicked = event.target.innerHTML;
-        console.log(buttonClicked);
         stopTimer();
-        if (buttonClicked == questions[0].answer) {
+        $('.answer-button').css("display", "none");
+        if (buttonClicked == questions[questionCounter].answer) {
             correct++;
+            console.log("correct = " + correct);
             $('.right-or-wrong').html("Correct!");
-            $('.gif-area').attr("src", gifArrCorrect[0]);
+            $('.gif-area').attr("src", gifArrCorrect[gifCounter]);
             $('.gif-area').css("display", "inline-block");
         } else {
-            $('.right-or-wrong').html("Sorry. The answer was " + questions[0].answer);
-            $('.gif-area').attr("src", gifArrWrong[0]);
+            incorrect++;
+            console.log("incorrect = " + incorrect);
+            $('.right-or-wrong').html("Sorry. The answer was " + questions[questionCounter].answer + ".");
+            $('.gif-area').attr("src", gifArrWrong[gifCounter]);
             $('.gif-area').css("display", "inline-block");
         }
-        setTimeout(function () { setQA(); }, 5000);
+        // moved from 105
+        delay = setTimeout(function () { setQA(); }, 5000);
+        checkScore(); 
+        gifCounter++;
+        questionCounter++;
     }); // end of answer button being clicked
 
-
-
-
-
+    function checkScore() {
+        if ((correct + incorrect + timedOut) === questions.length) {
+            clearTimeout(delay);
+            $('.question-text').css("display", "none");
+            //$('#time-counter').css("display", "none");
+            $('.answer-button').css("display", "none");
+            $('.reset-button').css("display", "inline-block");
+            $('.correct').html("Correct: " + correct);
+            $('.incorrect').html("Incorrect: " + correct);
+            $('.timedout').html("Timed Out: " + correct);
+            $('.correct').css("display", "inline-block");
+            $('.incorrect').css("display", "inline-block");
+            $('.timedout').css("display", "inline-block");
+            $('.answer-button').css("display", "none");
+            //return;
+        }
+    }
 }); // end of document ready function
 
 // display h1 and button
